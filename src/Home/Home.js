0f8ac466586a0c3fd4da10eaaa4223854216a277
hassanosamaa <almsryh877@gmail.com>
2023-12-16 13:05:19 +0200

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,39 @@
+export default function Home(){
+    return(
+        <div className="Home Flex">
+
+            <div className="back"></div>
+            <div className="left ">
+
+                <div className="leftimg1">
+                    <div className="leftimgs1"></div>
+                    <div className="leftimgsc1"></div>
+
+                </div>
+                <div className="leftimg">
+                    <div className="leftimgs"></div>
+                    <div className="leftimgsc"></div>
+                </div>
+            
+                
+            </div>
+
+            <div className="right Flex">
+                <p className="top">CONCRETE</p>
+                <p className="bottom">DESIGN & TRAINING</p>
+                <p className="pra1">نقدم لك مجموعة متنوعة من أقوى كورسات الهندسة المدنية والمعمارية
+يمكنك الاشتراك خلال دقائق من أي مكان بالعالم</p>
+                <p className="pra2">والحصول على فيديوهات مسجلة تستطيع مشاهدتها في أي وقت
+على اللاب توب أو تطبيق الهاتف بدون الحاجة لإنترنت</p>
+
+                <div className="icons">
+                <a href="https://www.facebook.com/CivilCoursesEngineering"><i className="fa fa-brands fa-facebook fa-2xl"></i></a>
+                <a href="https://wa.me/message/HKKYFIBQALB3N1"><i className="wat fa-brands fa-whatsapp fa-2xl"></i></a>
+                <a href="https://t.me/Concrete_Academy"><i className=" tel fa-brands fa-telegram fa-2xl"></i></a>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Header(){
+
+
+    const [x,y]=useState(true);
+    const body=document.getElementById("body");
+    function dark()
+    {
+
+       y((v)=>!v);
+        
+        body.classList.toggle("dark");
+    }
+    return(
+        <div className="HeaderBox Flex">
+           
+            <div className="change Flex"><a href="#"> <i  className={ x?" change2 fa-solid fa-moon fa-2xl":"change2 fa-solid fa-sun fa-2xl"}
+             onClick={dark} > </i></a></div>
+
+
+            <ul className="nav Flex" >
+                <li> <Link className="Link">المحاضرين</Link></li>
+                <li> <Link className="Link">الأقسام</Link></li>
+                <li> <Link className="Link">الكورسات المتاحة</Link></li>
+                <li> <Link  className="Link active">الصفحة الشخصية</Link></li>
+                
+            </ul>
+
+            <div className="logo Flex">
+                
+                <img src={require("../logooo.png")}
+                alt="logo"
+                className="logoo "
+                
+                />
+            </div>
+        </div>
+    )
+}
